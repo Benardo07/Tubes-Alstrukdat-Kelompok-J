cfd79a7150b68d868c2c4418942d1a6f790a77f0
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-11-14 01:50:31 +0700

    refactor: Fix bug on profil feature

--- profil/profil.c
+++ profil/profil.c
@@ -1,18 +1,31 @@
 # include <stdio.h>
-# include "ADT/matriks/foto.h"
-# include "ADT/boolean.h"
-# include "ADT/liststatik/listpengguna.h"
-# include "main.h"
+# include "profil.h"
 
 void setBio(){
+    printf("Masukkan Bio Akun (Max : 135 karakter): \n");
     StartSentence();
-    strCpy(currentWord.TabWord,BIO(currentUser));
+    int i;
+    char* s = currentWord.TabWord;
+    for(i=0;i<listLengthP(LPengguna);i++){
+        if(ID(ELMT(LPengguna,i)) == IdCurrentUser){
+            strCpy(s,BIO(ELMT(LPengguna,i)));
+            while(strlength(s) > 135){
+                printf("Bio memiliki panjang melebihi 135 karakter. Masukkan sesuai format lagi : \n");
+                StartSentence();
+                s = currentWord.TabWord;
+                strCpy(s,BIO(ELMT(LPengguna,i)));
+            }
+        }
+    }
 }
 
 boolean NumberValid(Word input){
     int i;
-    for(i=0;i<input.Length;i++){
-        if(input.TabWord[i] != "0" || input.TabWord[i] != "1" || input.TabWord[i] != "2" || input.TabWord[i] != "3" || input.TabWord[i] != "4" || input.TabWord[i] != "5" || input.TabWord[i] != "6" || input.TabWord[i] != "7" || input.TabWord[i] != "8" || input.TabWord != "9"){
+    for(i = 0; i < input.Length; i++){
+        if(input.TabWord[i] != '0' && input.TabWord[i] != '1' && input.TabWord[i] != '2' && 
+           input.TabWord[i] != '3' && input.TabWord[i] != '4' && input.TabWord[i] != '5' && 
+           input.TabWord[i] != '6' && input.TabWord[i] != '7' && input.TabWord[i] != '8' && 
+           input.TabWord[i] != '9'){
             return false;
         }
     }
@@ -22,40 +35,67 @@ boolean NumberValid(Word input){
 void setNoHP(){
     printf("Masukkan No HP: \n");
     StartSentence();
-    strCpy(currentWord.TabWord,HP(currentUser));
-    while(!NumberValid(currentWord)){
-        printf("\n");
-        printf("No HP tidak valid. Masukkan lagi yuk!\n");
-        printf("Masukkan No HP: \n");
-        StartSentence();
-        strCpy(currentWord.TabWord,HP(currentUser));
-    }
+    char* hp;
+    char* hpnew;
+    hp = currentWord.TabWord;
+    int i;
+    for(i=0;i<listLengthP(LPengguna);i++){
+        if(ID(ELMT(LPengguna,i)) == IdCurrentUser){
+            strCpy(hp,HP(ELMT(LPengguna,i)));
+            while(!NumberValid(currentWord)){
+                printf("\n");
+                printf("No HP tidak valid. Masukkan lagi yuk!\n");
+                printf("\n");
+                printf("Masukkan No HP: \n");
+                StartSentence();
+                hpnew = currentWord.TabWord;
+                strCpy(hpnew,HP(ELMT(LPengguna,i)));
+            }
+        }
+    } 
 }
 
 void setWeton(){
     printf("Masukkan Weton: \n");
     STARTWORD();
-    strCpy(currentWord.TabWord,WETON(currentUser));
-    while(!isWeton(currentWord.TabWord)){
-        printf("\n");
-        printf("Weton anda tidak valid.\n");
-        printf("Masukkan Weton: \n");
-        StartSentence();
-        strCpy(currentWord.TabWord,HP(currentUser));
+    char* weton = currentWord.TabWord;
+    char* wetonNew;
+    int i;
+    for(i=0;i<listLengthP(LPengguna);i++){
+        if(ID(ELMT(LPengguna,i)) == IdCurrentUser){
+            strCpy(weton,WETON(ELMT(LPengguna,i)));
+            while(!isWeton(weton)){
+                printf("\n");
+                printf("Weton anda tidak valid.\n");
+                printf("\n");
+                printf("Masukkan Weton: \n");
+                StartSentence();
+                wetonNew = currentWord.TabWord;
+                strCpy(wetonNew,WETON(ELMT(LPengguna,i)));
+            }
+        }
     }
 }
 
 void gantiProfil(){
-    printf("| Nama: %c\n",currentUser.nama);
+    printf("| Nama: %s\n",NAMA(ELMT(LPengguna,IdCurrentUser)));
     printf("| Bio Akun: \n");
     printf("| No HP: \n");
     printf("| Weton: \n");
     printf("\n");
 
-    printf("Masukkan Bio Akun: \n");
     setBio();
+    printf("\n");
     setNoHP();
+    printf("\n");
     setWeton();
+    printf("\n");
+
+    printf("| Nama: %s\n", NAMA(ELMT(LPengguna,IdCurrentUser)));
+    printf("| Bio Akun: %s\n", BIO(ELMT(LPengguna,IdCurrentUser)));
+    printf("| No HP: %s\n", HP(ELMT(LPengguna,IdCurrentUser)));
+    printf("| Weton: %s\n", WETON(ELMT(LPengguna,IdCurrentUser)));
+    printf("\n");
     
     printf("Profil Anda sudah berhasil diperbarui!\n");
 }
@@ -63,17 +103,20 @@ void gantiProfil(){
 void LihatProfil(char *s){
     int id;
     id = searchNama(LPengguna,s);
+    printf("%d\n",id);
     if(JENIS(ELMT(LPengguna,id)) == "Privat"){
-        printf("Wah, akun %c diprivat nih. Ikuti dulu yuk untuk bisa melihat profil %c!", s);
+        printf("Wah, akun %s diprivat nih. Ikuti dulu yuk untuk bisa melihat profil %s!", s);
+        return;
     };
 
-    printf(" | Nama: %c\n", NAMA(ELMT(LPengguna,id)));
-    printf(" | Bio Akun: %c\n", BIO(ELMT(LPengguna,id)));
-    printf(" | No HP: %c\n", HP(ELMT(LPengguna,id)));
-    printf(" | Weton: %c\n", WETON(ELMT(LPengguna,id)));
-    
+    printf("| Nama: %s\n", NAMA(ELMT(LPengguna,id)));
+    printf("| Bio Akun: %s\n", BIO(ELMT(LPengguna,id)));
+    printf("| No HP: %s\n", HP(ELMT(LPengguna,id)));
+    printf("| Weton: %s\n", WETON(ELMT(LPengguna,id)));
+    printf("\n");
     printf("Foto profil akun TuanBri\n");
     PrintPhotoMatrix(FOTO(ELMT(LPengguna,id)));
+    printf("\n");
 }
 
 void aturjenisakun (){
@@ -81,23 +124,41 @@ void aturjenisakun (){
     printf("(YA/TIDAK) ");
     STARTWORD();
     printf("\n");
-    char *choice;
-    strCpy(currentWord.TabWord, choice);
-    if(isStrEqual(choice,"Privat")){
-        strCpy("Publik",JENIS(currentUser));
+    char *choice = currentWord.TabWord;
+    if(isStrEqual(choice,"YA") || isStrEqual(choice,"ya") || isStrEqual(choice,"Ya")){
+        strCpy("Privat",JENIS(ELMT(LPengguna,IdCurrentUser)));
         printf("Akun anda telah diubah menjadi akun Privat.\n");
     }else{
-        strCpy("Publik",JENIS(currentUser));
         printf("Akun anda tetap menjadi akun Publik. \n");
     }
 }
 
-void UbahfotoProfil(currentuser){
-    printf("Foto profil anda saat ini \n");
-    PrintPhotoMatrix(FOTO(currentUser));
+int isSpaceOrControl(char ch) {
+    return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' || (ch >= 0 && ch <= 31);
+}
 
-    printf("Masukkan foto profil yang baru: \n");
+void UbahfotoProfil() {
+    printf("\nFoto profil anda saat ini adalah\n");
+    PrintPhotoMatrix(FOTO(ELMT(LPengguna,IdCurrentUser)));
 
-    printf("Foto Profil Anda sudah berhasil diganti!\n");
+    printf("Masukkan foto profil yang baru\n");
+    STARTWORD();  
+    char* input = currentWord.TabWord;
+
+    int row = 0, col = 0, i = 0;
+    while (input[i] != '\0' && input[i] != ';') {
+        if (!isSpaceOrControl(input[i])) {
+            if (col < COL_EFF(FOTO(ELMT(LPengguna,IdCurrentUser)))) {
+                ChangePhotoMatrix(&FOTO(ELMT(LPengguna,IdCurrentUser)), row, col, input[i]);
+                col++;
+            }
+        } else if (input[i] == '\n' || input[i] == '\r') {
+            row++;
+            col = 0;
+        }
+        i++;
     }
-
+    printf("\n");
+    printf("Foto Profil Anda sudah berhasil diganti!\n");
+    PrintPhotoMatrix(FOTO(ELMT(LPengguna,IdCurrentUser)));
+}
---
+++ profil/profil.h
@@ -0,0 +1,18 @@
+# include "../ADT/matriks/foto.h"
+# include "../ADT/boolean.h"
+# include "../ADT/liststatik/listpengguna.h"
+# include "../data/data.h"
+
+void setBio();
+
+void setNoHP();
+
+void setWeton();
+
+void gantiProfil();
+
+void LihatProfil(char *s);
+
+void aturjenisakun();
+
+void UbahfotoProfil();
