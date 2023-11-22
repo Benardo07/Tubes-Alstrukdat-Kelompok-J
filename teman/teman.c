796c4a985d14a3c9b2dc12cf40fdacb55bfd59a1
IrfanSidiq <irvansidik64@gmail.com>
2023-11-22 17:30:22 +0700

    fix teman pake currentSencence

--- teman/teman.c
+++ teman/teman.c
@@ -25,6 +25,18 @@ void daftarTeman(int idUser) {
             DequeuePrio(&q, &temp);
         }
     }
+
+    // if (TOTALFRIENDS(ELMT(LPengguna, idUser-1)) == 0) {
+    //     printf("%s belum mempunyai teman.\n", NAMA(currentUser));
+    // }
+    // else {
+    //     printf("%s mempunyai %d teman\n", NAMA(currentUser), TOTALFRIENDS(ELMT(LPengguna, idUser-1)));
+    //     for (int i=0; i<SIMPUL(Teman); ++i) {
+    //         if (i != IdCurrentUser-1 && isTeman(i+1, IdCurrentUser)) {
+    //             printf("| %s\n", NAMA(ELMT(LPengguna, i)));
+    //         }
+    //     }
+    // }
 }
 
 int searchNamaP(ListPengguna l, char *nama){
@@ -46,7 +58,7 @@ void hapusTeman() {
     printf("Masukkan nama pengguna: ");
     char nama[21];
     StartSentence();
-    strCpy(currentWord.TabWord, nama);
+    strCpy(currentSentence.TabWord, nama);
 
     int id_target = searchNamaP(LPengguna, nama);
     if (id_target != IDX_UNDEF) {
@@ -59,13 +71,13 @@ void hapusTeman() {
             boolean inputValid = false;
             while(!inputValid) {
                 StartSentence();
-                if (isStrEqual(currentWord.TabWord, "YA")) {
+                if (isStrEqual(currentSentence.TabWord, "YA")) {
                     deleteEdge(&Teman, ID(currentUser)-1, id_target-1);
                     printf("%s berhasil dihapus dari daftar teman Anda.\n", nama);
                     inputValid = true;
                     TOTALFRIENDS(currentUser)--;
                 }
-                else if (isStrEqual(currentWord.TabWord, "TIDAK")) {
+                else if (isStrEqual(currentSentence.TabWord, "TIDAK")) {
                     printf("Penghapusan teman dibatalkan.\n");
                     inputValid = true;
                 }
